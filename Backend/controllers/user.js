a43ae7e859d6c1e74d6f10db650ec84137f00a71
mdnishanchowdhury <mdnishanchowdhury.cse@gmail.com>
2025-04-14 14:29:35 +0600

    structured the code to use controllers

---
+++ Backend/controllers/user.js
@@ -0,0 +1,23 @@
+const User = require("../models/User");
+
+//Deleting User
+const deleteUser = async (req, res) => {
+  try {
+    await User.findByIdAndDelete(res.params.id);
+    res.status(201).json("The user has been deleted successfully");
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
+
+//get all users
+
+const getAllUsers = async (req, res) => {
+  try {
+    const users = await User.find.sort({ createdAt: -1 });
+    res.status(200).json(users);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
+module.exports = { getAllUsers, deleteUser };
